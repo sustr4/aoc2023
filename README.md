33675ed310e7ac168709a9a4765e0ca31ec2eab4
Zdenek Sustr <git@sustr.net>
2023-11-30 21:12:43 +0100

    Preparation for December

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# aoc2023
-Advent of Code 2023
+
+This is a repository of my solutions for [Advent of Code 2023](https://adventofcode.com/2023). Typically there is code showing only solutions of the 2nd task. Occasionally I remember to commit Task 1 solution before I modify the code, so it might be found in the history.
+
---
+++ init/day00.c
@@ -0,0 +1,116 @@
+#define _GNU_SOURCE
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<limits.h>
+#include<assert.h>
+
+// Boundary definitions, set as required
+#define MAXX 76
+#define MAXY 26
+
+// Point structure definition
+typedef struct {
+	int x;
+	int y;
+	int z;
+} TPoint;
+
+// Comparator function example
+int comp(const void *a, const void *b)
+{
+	const int *da = (const int *) a;
+	const int *db = (const int *) b;
+	return (*da > *db) - (*da < *db);
+}
+
+// Example for calling qsort()
+//qsort(array,count,sizeof(),comp);
+
+
+// Print a two-dimensional array
+void printMap (char **map) {
+	int x,y;
+	for(y=0; y<MAXY; y++) {
+		for(x=0; x<MAXX; x++) {
+			printf("%c", map[y][x]);
+		}
+		printf("\n");
+	}
+}
+// Full block character for maps █
+
+
+// Read input file line by line (e.g., into an array)
+TPoint *readInput() {
+//int readInput() {
+	FILE * input;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int count = 0;
+
+	input = fopen("input.txt", "r");
+	if (input == NULL) {
+		fprintf(stderr,"Failed to open input file\n");
+		exit(1); }
+
+	// Allocate one-dimensional array of strings
+	// char **inst=(char**)calloc(MAXX, sizeof(char*));
+	// TPoint *inst=(TPoint*)calloc(MAXX, sizeof(TPoint));
+
+	// Allocate a two-dimensional arrray of chars
+	// int x=0, y=0;
+	// char **map=calloc(MAXY,sizeof(char*));
+	// for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
+
+	while ((read = getline(&line, &len, input)) != -1) {
+		line[strlen(line)-1] = 0; // Truncate the NL
+
+		// Read into map
+		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
+		// y++;
+
+		// Copy to string
+		//asprintf(&(inst[count]), "%s", line);	
+
+		// Read into array
+		// sscanf(line,"%d,%d",
+		//	&(inst[count].x),
+		//	&(inst[count].y));
+
+		// Read tokens from single line
+		//char *token;
+		//token = strtok(line, ",");
+		//while( 1 ) {
+		//	if(!(token = strtok(NULL, ","))) break;
+		//}
+
+		count++;
+	}
+
+	fclose(input);
+	if (line)
+	free(line);
+
+//	printMap(map);
+
+	return 0;
+//	return inst;
+//	return map;
+}
+
+int main(int argc, char *argv[]) {
+
+//	TPoint *array;
+//	int i=0;	
+//	array = readInput();
+	readInput();
+
+//	for(i=0; array[i]; i++) {
+//		printf("%d\n", array[i]);
+//	}
+
+	return 0;
+}
