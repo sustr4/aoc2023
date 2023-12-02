3e667be1ffbbb1ed938a07353c84713a2e1c3e3b
Zdenek Sustr <git@sustr.net>
2023-12-02 06:27:40 +0100

    Day 2, task 2

--- day02/day02.sh
+++ day02/day02.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 INPUT="input.txt"
+#INPUT="unit1.txt"
 
 #12 red cubes, 13 green cubes, and 14 blue cubes
 
 SUM=0
+
 cat $INPUT | while read line; do
 
+	MR=0
+	MG=0
+	MB=0
 
 	NUM=`echo $line | awk -F"[ :]" '{print $2}'`
 
-	echo $NUM $line
+	echo l.${NUM}: $line
 
 	RED=`echo $line | grep -o '[0-9]*\sred' | awk '{print $1}'`
 	GREEN=`echo $line | grep -o '[0-9]*\sgreen' | awk '{print $1}'`
 	BLUE=`echo $line | grep -o '[0-9]*\sblue' | awk '{print $1}'`
 
-	echo $RED
-	echo $GREEN
-	echo $BLUE
+	echo Reds: $RED 
+	echo Greens: $GREEN
+	echo Blues: $BLUE
 
 	LIN=$NUM
 
-	for i in $RED; do
-		if [ $i -gt 12 ]; then LIN=0; fi
-	done
-	for i in $GREEN; do
-		if [ $i -gt 13 ]; then LIN=0; fi
-	done
-	for i in $BLUE; do
-		if [ $i -gt 14 ]; then LIN=0; fi
-	done
-	echo $LIN
-
-	SUM=$(( $SUM + $LIN))
-	echo $SUM
+	for i in $RED; do if [ $i -gt $MR ]; then MR=$i; fi done
+	for i in $GREEN; do if [ $i -gt $MG ]; then MG=$i; fi done
+	for i in $BLUE; do if [ $i -gt $MB ]; then MB=$i; fi done
+	echo Maxes: $MR $MG $MB
+	
+	POW=$(( $MR * $MG * $MB ))
+	SUM=$(( $SUM + $POW ))
+	echo Power ${POW}, running sum: $SUM
 done
