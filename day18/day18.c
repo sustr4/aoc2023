b77aefc20e2658828609712153fef38287017df9
Zdenek Sustr <git@sustr.net>
2023-12-18 18:14:03 +0100

    Day 18, task 1 again but this time without memory consumption.

--- day18/day18.c
+++ day18/day18.c
@@ -7,11 +7,12 @@
 #include<assert.h>
 
 // Boundary and input file definitions, set as required
-//#define INPUT "input.txt"
-#define INPUT "unit1.txt"
+#define INPUT "input.txt"
+//#define INPUT "unit1.txt"
+//#define INPUT "unit2.txt"
 #define MAXOP 800
-#define MAXX 100
-#define MAXY 100
+#define MAXX 1000
+#define MAXY 1000
 
 // Point structure definition
 typedef struct {
@@ -20,7 +21,13 @@ typedef struct {
 	int dy;
 	int count;
 	char *color;
+	char dir2;
+	int count2;
 } TOp;
+typedef struct {
+	int x;
+	int y;
+} TPoint;
 
 int ymin=0, ymax=0, xmin=0, xmax=0;
 
@@ -39,8 +46,8 @@ int comp(const void *a, const void *b)
 // Print a two-dimensional array
 void printMap (char **map) {
 	int x,y;
-	for(y=ymax; y<=ymin; y++) {
-		for(x=xmax; x<=xmin; x++) {
+	for(y=ymin; y<=ymax; y++) {
+		for(x=xmin; x<=xmax; x++) {
 			if( map[y][x]) 	printf("%c", map[y][x]);
 			else printf(" ");
 		}
@@ -91,26 +98,34 @@ TOp *readInput() {
 		// Copy to string
 		//asprintf(&(inst[count]), "%s", line);	
 
-		// Read into array
-		inst[count].color=calloc(10, sizeof(char));
-		sscanf(line,"%c %d %s",
-			&(inst[count].dir),
-			&(inst[count].count),
-			inst[count].color);
+		// Read tokens from single line
+		char *token;
+		token = strtok(line, " ");
+		inst[count].dir=token[0];
+		token = strtok(NULL, " ");
+		inst[count].count=atoi(token);
+		token = strtok(NULL, " ");
+		inst[count].color = strdup(token);
+
+		printf("%s\n", inst[count].color);
 
 		switch(inst[count].dir) {
+		case '0':
 		case 'R':
 			inst[count].dx=1;
 			inst[count].dy=0;
 			break;
+		case '2':
 		case 'L':
 			inst[count].dx=-1;
 			inst[count].dy=0;
 			break;
+		case '3':
 		case 'U':
 			inst[count].dx=0;
 			inst[count].dy=-1;
 			break;
+		case '1':
 		case 'D':
 			inst[count].dx=0;
 			inst[count].dy=1;
@@ -120,12 +135,10 @@ TOp *readInput() {
 			break;
 		}
 
-		// Read tokens from single line
-		//char *token;
-		//token = strtok(line, ",");
-		//while( 1 ) {
-		//	if(!(token = strtok(NULL, ","))) break;
-		//}
+		inst[count].dir2=inst[count].color[7];
+		inst[count].color[7]=0;
+		inst[count].count2=strtol(inst[count].color+2, NULL, 16);
+
 
 		count++;
 	}
@@ -140,14 +153,28 @@ TOp *readInput() {
 
 void colorIn (char **map) {
 	int y, x;
-	for(y=ymax; y<=ymin; y++) {
-		if(map[y][0]==0) map[y][0]='.';
-		if(map[y][xmin]==0) map[y][xmin]='.';
+	map[xmin][ymin]='.';
+	while(1) {
+		int change=0;
+
+		for(y=0; y<MAXY; y++) {
+			for(x=0; x<=MAXX; x++) {
+				if(map[y][x]!=0) continue;
+				for(int n=0; n<8; n++) {
+					if(mapnb(map, y, x, n)=='.') {
+						map[y][x]='.';
+						change=1;
+					}
+				}
+			}
+		}
+		if(!change) break;
 	}
-	for(x=xmax; x<=xmin; x++) {
-		if(map[0][x]==0) map[0][x]='.';
-		if(map[ymin][x]==0) map[ymin][x]='.';
-	}
+		for(y=ymin; y<=ymax; y++) {
+			for(x=xmin; x<=xmax; x++) {
+				if(!map[y][x]) map[y][x]='#';
+			}
+		}
 }
 
 int main(int argc, char *argv[]) {
@@ -157,26 +184,53 @@ int main(int argc, char *argv[]) {
 	char **map=calloc(MAXY,sizeof(char*));
 	for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
 
+	TPoint *polygon=(TPoint*)calloc(MAXOP, sizeof(TPoint));
+
 //	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
-	int x = MAXX / 2;
-	int y = MAXY / 2;
+	int x = 0;
+	int y = 0;
 	xmin=x; xmax=x;
 	ymin=y; ymax=y;
+	int pt=0;
+	polygon[pt].x=x;
+	polygon[pt].y=y;
+	int halves=0;
 	for(i=0; array[i].dir; i++) {
-		printf("%c %d %s\n", array[i].dir, array[i].count, array[i].color);
-		for(int q=0; q<array[i].count; q++) {
-			x+=array[i].dx;
-			y+=array[i].dy;
-			map[y][x]='#';
-			if(ymin<y) ymin=y;
-			if(ymax>y) ymax=y;
-			if(xmin<x) xmin=x;
-			if(xmax>x) xmax=x;
-		}
+		printf("%c %d %s T2: %c %d\n", array[i].dir, array[i].count, array[i].color, array[i].dir2, array[i].count2);
+		x+=array[i].count*array[i].dx;
+		y+=array[i].count*array[i].dy;
+		if(ymin>y) ymin=y;
+		if(ymax<y) ymax=y;
+		if(xmin>x) xmin=x;
+		if(xmax<x) xmax=x;
+		pt++;
+		polygon[pt].x=x;
+		polygon[pt].y=y;
+		halves+=array[i].count;
 	}
-	printMap(map);
 
-	printf("Dimensions: %d -- %d horizontally, %d -- %d vertically\n", xmin, xmax, ymax, ymin);
+	printf("Dimensions: %d -- %d horizontally, %d -- %d vertically\n", xmin, xmax, ymin, ymax);
+
+//	TPoint polygon[]={{0, 0}, {3,0}, {3,3}, {0,3}, {0,0}};
+//	pt=4;
+
+	int a=0;
+	int b=0;
+
+	for(int p=pt; p>0; p--) {
+		printf("(%3d,%3d)\t", polygon[p].x, polygon[p].y);
+		a+=polygon[p].x*polygon[p-1].y;
+		printf("%3d x %3d\t", polygon[p].x, polygon[p-1].y);
+		b+=polygon[p].y*polygon[p-1].x;
+		printf("%3d x %3d\n", polygon[p].y, polygon[p-1].x);
+	}
+	printf("(%3d,%3d)\n", polygon[0].x, polygon[0].y);
+
+	printf("A = %d, B = %d, A-B = %d, half = %d, + %d halves = %d\n", a, b, a-b, (a-b)/2, halves,  abs((a-b)/2) + halves/2 + 1);
+
+//	printMap(map);
+//	colorIn(map);
+//	printMap(map);
 
 	return 0;
 }
