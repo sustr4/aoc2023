d8581195cfb1d814e4f21fc6b93addda497ec3ff
Zdenek Sustr <git@sustr.net>
2023-12-18 05:58:52 +0100

    Progress, not done

--- day17/day17.c
+++ day17/day17.c
@@ -7,12 +7,14 @@
 #include<assert.h>
 
 // Boundary and input file definitions, set as required
-#define INPUT "input.txt"
-#define MAXX 141
-#define MAXY 141
-//#define INPUT "unit1.txt"
-//#define MAXX 13
-//#define MAXY 13
+//#define INPUT "input.txt"
+//#define MAXX 141
+//#define MAXY 141
+#define INPUT "unit1.txt"
+#define MAXX 13
+#define MAXY 13
+
+#define MAXSTEP 10
 
 int startMax=INT_MAX;
 
@@ -21,18 +23,6 @@ typedef struct {
 	int x;
 	int y;
 } TPoint;
-
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-	const int *da = (const int *) a;
-	const int *db = (const int *) b;
-	return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
 
 // Print a two-dimensional array
 void printMap (int **map) {
@@ -46,23 +36,13 @@ void printMap (int **map) {
 }
 // Full block character for maps █ and border elements ┃━┗┛┏┓
 // Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
-void printScore (int **map, int ****score) {
+void printScore (int **map, int ***score) {
 	int x,y;
-	int **went=calloc(MAXY,sizeof(int*));
-	for(int iter=0; iter<MAXY; iter++) went[iter]=calloc(MAXX,sizeof(int));
-
 
 	for(y=0; y<MAXY; y++) {
 		for(x=0; x<MAXX; x++) {
-                       int min=INT_MAX;
-                       for(int d=0; d<4; d++) {
-                               for(int c=1; c<=3; c++) {
-                                       if((score[y][x][d][c]) && (score[y][x][d][c]<min)) min=score[y][x][d][c];
-                               }
-                       }
-			if(went[y][x]) printf("\033[1;34m");
-                       if(min==INT_MAX) printf("___");
-                       else printf("%3d", min);
+			printf("\033[1;34m%3d", score[y][x][0]-map[0][0]);
+			printf("\033[1;32m%3d", score[y][x][1]-map[0][0]);
 			printf("\033[0;31m%d\033[0m ", map[y][x]);
 		}
 		printf("\n");
@@ -142,58 +122,6 @@ int newy(int y, int steps, int d) { // New X based on direction
 	return y;
 }
 
-int step(int x, int y, int d, int steps, int **map, int ****score, int depth, int s) {
-
-//	static int iteration=0;
-	int nx, ny, nd;
-
-//	if(startMax<136) return 0;
-//	iteration++;
-//	if(iteration>8000) return 0;
-
-//	printf("\nIteration %d: [%d,%d], direction %d, depth %d, score %d\n", iteration, x, y, d, depth, s);
-	if((y<0) || (x<0) || (y>=MAXY) || (x>=MAXX)) return 0;
-
-	if(s>startMax) return 0;
-
-	if((score[y][x][d][steps]) && (s>=score[y][x][d][steps])) return 0; // Already been here with a better score
-
-	score[y][x][d][steps]=s; // This is the best score on the way here so far
-
-	if((y==MAXY-1) && (x==MAXX-1)) {
-		startMax=s;
-		printf("Found better minimum: %d\n", startMax);
-		printScore(map,score);
-		return 0;
-	}
-
-	// Try left
-	nd=d-1; if(nd<0) nd=3;
-	int sadd=0;
-	for(int stepNo=1; stepNo<=3; stepNo++) {
-//		printf("Trying %d left\n", stepNo);
-		nx=newx(x, stepNo, nd); ny=newy(y, stepNo, nd);
-		if((ny<0) || (nx<0) || (ny>=MAXY) || (nx>=MAXX)) break;
-		sadd+=map[ny][nx];
-//		printf("Adding %d to score (=%d)\n", sadd, s+sadd);
-		step(nx, ny, nd, stepNo, map, score, depth+1, s+sadd);
-	}
-
-	// Try right
-	nd=d+1; if(nd>3) nd=0;
-	sadd=0;
-	for(int stepNo=1; stepNo<=3; stepNo++) {
-//		printf("Trying %d right\n", stepNo);
-		nx=newx(x, stepNo, nd); ny=newy(y, stepNo, nd);
-		if((ny<0) || (nx<0) || (ny>=MAXY) || (nx>=MAXX)) break;
-		sadd+=map[ny][nx];
-//		printf("Adding %d to score\n", sadd);
-		step(nx, ny, nd, stepNo, map, score, depth+1, s+sadd);
-	}
-
-	return 0;
-}
-
 
 
 int main(int argc, char *argv[]) {
@@ -203,41 +131,97 @@ int main(int argc, char *argv[]) {
 //	array = readInput();
 	int **map=readInput();
 
-        int ****score=calloc(MAXY,sizeof(int***));
+        int ***score=calloc(MAXY,sizeof(int**));
         for(int y=0; y<MAXY; y++) {
-                score[y]=calloc(MAXX,sizeof(int**));
+                score[y]=calloc(MAXX,sizeof(int*));
                 for(int x=0; x<MAXX; x++) {
-                        score[y][x]=calloc(4,sizeof(int*));
-                        for(int d=0; d<4; d++) {
-                                score[y][x][d]=calloc(4,sizeof(int));
-                        }
+                        score[y][x]=calloc(2,sizeof(int));
+                }
+        }
+        int ***step=calloc(MAXY,sizeof(int**));
+        for(int y=0; y<MAXY; y++) {
+                step[y]=calloc(MAXX,sizeof(int*));
+                for(int x=0; x<MAXX; x++) {
+                        step[y][x]=calloc(2,sizeof(int));
                 }
         }
 
-	startMax=map[0][0];
+	score[0][0][0]=map[0][0];
+	score[0][0][1]=map[0][0];
+	step[0][0][0]=1;
+	step[0][0][1]=1;
 
-	int l=0;
-	int y=0, x=0;
-	while(1) {
-		if(l) { x++; l=!l; }
-		else { y++; l=!l; }
-		startMax+=map[y][x];
-		if((x==MAXX-1) && (y=MAXY-1)) break;
+	int dx, dy;
+	for(int s=1; s<200; s++) {
+		// from vertical to horizontal
+        	for(int y=0; y<MAXY; y++) {
+                	for(int x=0; x<MAXX; x++) {
+				if(step[y][x][0]!=s) continue; // Only expand the current step
+				int cumul=0;
+				for(dx=-1; dx>=0-MAXSTEP; dx--) { // Left
+					if(x+dx<0) break;
+					cumul+=map[y][x+dx];
+					if(!score[y][x+dx][1]) {
+						score[y][x+dx][1]=score[y][x][0]+cumul;
+						step[y][x+dx][1]=s+1;
+					}
+					else if(score[y][x+dx][1]>score[y][x][0]+cumul) {
+						score[y][x+dx][1]=score[y][x][0]+cumul;
+						step[y][x+dx][1]=s+1;
+					}
+				}
+				cumul=0;
+				for(dx=1; dx<=MAXSTEP; dx++) { // Right
+					if(x+dx>=MAXX) break;
+					cumul+=map[y][x+dx];
+//					printf("[%d,%d] %d\n", y, x+dx, score[y][x][1]);
+					if(!score[y][x+dx][1]) {
+						score[y][x+dx][1]=score[y][x][0]+cumul;
+						step[y][x+dx][1]=s+1;
+					}
+					else if(score[y][x+dx][1]>score[y][x][0]+cumul) {
+						score[y][x+dx][1]=score[y][x][0]+cumul;
+						step[y][x+dx][1]=s+1;
+					}
+				}
+			}
+		}
+		// from horizontal to vertical
+        	for(int y=0; y<MAXY; y++) {
+                	for(int x=0; x<MAXX; x++) {
+				if(step[y][x][1]!=s) continue; // Only expand the current step
+				int cumul=0;
+				for(dy=-1; dy>=0-MAXSTEP; dy--) { // Up
+					if(y+dy<0) break;
+					cumul+=map[y+dy][x];
+					if(!score[y+dy][x][0]) {
+						score[y+dy][x][0]=score[y][x][1]+cumul;
+						step[y+dy][x][0]=s+1;
+					}
+					else if(score[y+dy][x][0]>score[y][x][1]+cumul) {
+						score[y+dy][x][0]=score[y][x][1]+cumul;
+						step[y+dy][x][0]=s+1;
+					}
+				}
+				cumul=0;
+				for(dy=1; dy<=MAXSTEP; dy++) { // Down
+					if(y+dy>=MAXY) break;
+					cumul+=map[y+dy][x];
+//					printf("[%d,%d] %d\n", y, x+dx, score[y][x][1]);
+					if(!score[y+dy][x][0]) {
+						score[y+dy][x][0]=score[y][x][1]+cumul;
+						step[y+dy][x][0]=s+1;
+					}
+					else if(score[y+dy][x][0]>score[y][x][1]+cumul) {
+						score[y+dy][x][0]=score[y][x][1]+cumul;
+						step[y+dy][x][0]=s+1;
+					}
+				}
+			}
+		}
 	}
 
-	printf("naiive max: %d\n", startMax);
-
-//	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
-//	for(i=0; array[i]; i++) {
-//		printf("%d\n", array[i]);
-//	}
-
-	step(0,0,1,3,map,score,0,0);
-	step(0,0,2,3,map,score,0,0);
-
-
-//	printf("\n");
-//	printScore(map,score);
+	printScore(map,score);
 
 	return 0;
 }
