f90ddd8134d808ceb6387d52841f81d8e74ba500
Zdenek Sustr <git@sustr.net>
2023-12-18 18:32:03 +0100

    day18, task 2

--- day18/day18.c
+++ day18/day18.c
@@ -22,50 +22,13 @@ typedef struct {
 	int count;
 	char *color;
 	char dir2;
-	int count2;
+	long count2;
 } TOp;
 typedef struct {
-	int x;
-	int y;
+	long x;
+	long y;
 } TPoint;
 
-int ymin=0, ymax=0, xmin=0, xmax=0;
-
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-	const int *da = (const int *) a;
-	const int *db = (const int *) b;
-	return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
-// Print a two-dimensional array
-void printMap (char **map) {
-	int x,y;
-	for(y=ymin; y<=ymax; y++) {
-		for(x=xmin; x<=xmax; x++) {
-			if( map[y][x]) 	printf("%c", map[y][x]);
-			else printf(" ");
-		}
-		printf("\n");
-	}
-}
-// Full block character for maps █ and border elements ┃━┗┛┏┓
-// Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
-
-// Retrieve nth neighbor from a map
-int dy[] = { -1, -1, -1, 0, 0, 1, 1, 1};
-int dx[] = { -1, 0, 1, -1, 1, -1, 0, 1};
-char mapnb(char **map, int y, int x, int n) {
-	assert((n>=0) && (n<8));
-	if((y+dy[n]<0) || (y+dy[n]>=MAXY) ||
-	   (x+dx[n]<0) || (x+dx[n]>=MAXX)) return 0;
-	return(map[y+dy[n]][x+dx[n]]);
-}
 
 // Read input file line by line (e.g., into an array)
 TOp *readInput() {
@@ -83,20 +46,8 @@ TOp *readInput() {
 	// Allocate one-dimensional array of strings
 	TOp *inst=(TOp*)calloc(MAXOP, sizeof(TOp));
 
-	// Allocate a two-dimensional arrray of chars
-	// int x=0, y=0;
-	// char **map=calloc(MAXY,sizeof(char*));
-	// for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
-
 	while ((read = getline(&line, &len, input)) != -1) {
 		line[strlen(line)-1] = 0; // Truncate the NL
-
-		// Read into map
-		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
-		// y++;
-
-		// Copy to string
-		//asprintf(&(inst[count]), "%s", line);	
 
 		// Read tokens from single line
 		char *token;
@@ -109,7 +60,8 @@ TOp *readInput() {
 
 		printf("%s\n", inst[count].color);
 
-		switch(inst[count].dir) {
+		inst[count].dir2=inst[count].color[7];
+		switch(inst[count].dir2) {
 		case '0':
 		case 'R':
 			inst[count].dx=1;
@@ -139,42 +91,12 @@ TOp *readInput() {
 		inst[count].color[7]=0;
 		inst[count].count2=strtol(inst[count].color+2, NULL, 16);
 
-
 		count++;
 	}
 
 	fclose(input);
 
-//	printMap(map);
-
 	return inst;
-//	return map;
-}
-
-void colorIn (char **map) {
-	int y, x;
-	map[xmin][ymin]='.';
-	while(1) {
-		int change=0;
-
-		for(y=0; y<MAXY; y++) {
-			for(x=0; x<=MAXX; x++) {
-				if(map[y][x]!=0) continue;
-				for(int n=0; n<8; n++) {
-					if(mapnb(map, y, x, n)=='.') {
-						map[y][x]='.';
-						change=1;
-					}
-				}
-			}
-		}
-		if(!change) break;
-	}
-		for(y=ymin; y<=ymax; y++) {
-			for(x=xmin; x<=xmax; x++) {
-				if(!map[y][x]) map[y][x]='#';
-			}
-		}
 }
 
 int main(int argc, char *argv[]) {
@@ -186,51 +108,35 @@ int main(int argc, char *argv[]) {
 
 	TPoint *polygon=(TPoint*)calloc(MAXOP, sizeof(TPoint));
 
-//	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
 	int x = 0;
 	int y = 0;
-	xmin=x; xmax=x;
-	ymin=y; ymax=y;
 	int pt=0;
 	polygon[pt].x=x;
 	polygon[pt].y=y;
-	int halves=0;
+	long halves=0;
 	for(i=0; array[i].dir; i++) {
-		printf("%c %d %s T2: %c %d\n", array[i].dir, array[i].count, array[i].color, array[i].dir2, array[i].count2);
-		x+=array[i].count*array[i].dx;
-		y+=array[i].count*array[i].dy;
-		if(ymin>y) ymin=y;
-		if(ymax<y) ymax=y;
-		if(xmin>x) xmin=x;
-		if(xmax<x) xmax=x;
+		printf("%c %d %s T2: %c %ld\n", array[i].dir, array[i].count, array[i].color, array[i].dir2, array[i].count2);
+		x+=array[i].count2*array[i].dx;
+		y+=array[i].count2*array[i].dy;
 		pt++;
 		polygon[pt].x=x;
 		polygon[pt].y=y;
-		halves+=array[i].count;
+		halves+=array[i].count2;
 	}
 
-	printf("Dimensions: %d -- %d horizontally, %d -- %d vertically\n", xmin, xmax, ymin, ymax);
-
-//	TPoint polygon[]={{0, 0}, {3,0}, {3,3}, {0,3}, {0,0}};
-//	pt=4;
-
-	int a=0;
-	int b=0;
+	long a=0;
+	long b=0;
 
 	for(int p=pt; p>0; p--) {
-		printf("(%3d,%3d)\t", polygon[p].x, polygon[p].y);
+		printf("(%9ld,%9ld)\t", polygon[p].x, polygon[p].y);
 		a+=polygon[p].x*polygon[p-1].y;
-		printf("%3d x %3d\t", polygon[p].x, polygon[p-1].y);
+		printf("%9ld x %9ld\t", polygon[p].x, polygon[p-1].y);
 		b+=polygon[p].y*polygon[p-1].x;
-		printf("%3d x %3d\n", polygon[p].y, polygon[p-1].x);
+		printf("%9ld x %9ld\n", polygon[p].y, polygon[p-1].x);
 	}
-	printf("(%3d,%3d)\n", polygon[0].x, polygon[0].y);
+	printf("(%9ld,%9ld)\n", polygon[0].x, polygon[0].y);
 
-	printf("A = %d, B = %d, A-B = %d, half = %d, + %d halves = %d\n", a, b, a-b, (a-b)/2, halves,  abs((a-b)/2) + halves/2 + 1);
-
-//	printMap(map);
-//	colorIn(map);
-//	printMap(map);
+	printf("A = %ld, B = %ld, A-B = %ld, half = %ld, + %ld halves = %ld\n", a, b, a-b, (a-b)/2, halves,  labs((a-b)/2) + halves/2 + 1);
 
 	return 0;
 }
