24b6f63aa0f088842275980de2fc19155d3e6a98
Zdenek Sustr <git@sustr.net>
2023-12-21 13:38:37 +0100

    Task 2 works, code needs cleaning

--- day21/day21.c
+++ day21/day21.c
@@ -110,8 +110,8 @@ char **readInput() {
 	return map;
 }
 
-int cnt(int qy, int qx, int **dist) {
-	int ret=0;
+long long cnt(int qy, int qx, int **dist) {
+	long long ret=0;
 
 	for(int y=MAXY*qy; y<MAXY*(qy+1); y++) {
 		for(int x=MAXX*qx; x<MAXX*(qx+1); x++) {
@@ -164,21 +164,108 @@ int main(int argc, char *argv[]) {
 //		printf("%d\n", array[i]);
 //	}
 
-	int count[7][7];
+	long long count[7][7];
 
 	int checksum=0;
 	for(int y=0; y<7; y++) {
+		int linesum=0;
 		for(int x=0; x<7; x++) {
 			count[y][x]=cnt(y, x, dist);
-			printf("%4d ", count[y][x]);
+			printf("%4lld ", count[y][x]);
+			linesum+=count[y][x];
 			checksum+=count[y][x];
 		}
-		printf("\n");
+		printf("\t%d\n", linesum);
 	}
 	
 
-	printf("Reached %d tiles.\n", checksum);
-	printf("26501365 is like %d. Go %d\n", 26501365 % MAXX, 26501365 % MAXX + MAXX);
+	printf("Reached %d testing tiles.\n", checksum);
+
+	int extend = 26501365 / MAXX ;
+
+	printf("Now .. we need %d tiles up, down, left right...\n", extend);
+
+//	extend=3; // TODO: Remove, this is for checking
+
+
+	long long totsum = 0;
+
+	int w = 3; //width
+	for(int y=0-extend; y<=extend; y++) {
+		long long sum = 0;
+//		sum = 0; // TODO: Remove, this is only for testing
+		if(y==0-extend) { // The first line
+			if(w<10) printf("f ");
+			for(int x=2; x<5; x++) sum+=count[0][x];
+		}
+		else if(y<0) { // Other lines in top half
+			w+=2;
+			if((w+1)%4) { // even lines
+				if(w<10) printf("e ");
+				sum+=count[2][0]+count[2][1]+count[2][5]+count[2][6];
+				sum+=(w-4) / 2 * count[1][2]; // Center and twins
+				sum+=((w-4) / 2 + 1) * count[1][3]; // Wing and twins
+			}
+			else { // odd lines
+				if(w<10) printf("o ");
+				sum+=count[1][1]+count[1][2]+count[1][4]+count[1][5];
+				sum+=(w-4) / 2 * count[2][3]; // Center and twins
+				sum+=((w-4) / 2 + 1) * count[2][2]; // Wing and twins
+			}
+		}
+		else if(y==0) { // Center line
+			printf("m ");
+			sum+=count[3][0]+count[3][1]+count[3][5]+count[3][6];
+			sum+=(w-4)/2 * count[3][3];
+			sum+=((w-4)/2 + 1) * count[3][2];
+		}
+		else if(y<extend) {
+			if(y>1) w-=2;
+			if(y%2==0) { // even lines
+				if(w<10) printf("e ");
+				sum+=count[5][1]+count[5][2]+count[5][4]+count[5][5];
+				sum+=(w-4) / 2 * count[3][2]; // Center and twins
+				sum+=((w-4) / 2 + 1) * count[3][3]; // Wing and twins
+			}
+			else { // odd lines
+				if(w<10) printf("o ");
+				sum+=count[4][0]+count[4][1]+count[4][5]+count[4][6];
+				sum+=(w-4) / 2 * count[4][3]; // Center and twins
+				sum+=((w-4) / 2 + 1) * count[4][2]; // Wing and twins
+			}
+		} else if(y==extend) {
+			if(w<10) printf("l ");
+			w-=2;
+			for(int x=2; x<5; x++) sum+=count[6][x];
+		}
+
+		if((w<10) || (y==0)) printf("Width %2d, Line sum %lld\n", w, sum);
+		totsum+=sum;
+	}
+
+	printf("Reached %lld tiles.\n", totsum);
+
+	long long dirsum = 0;
+
+	dirsum+=count[0][3]+extend*count[0][2]+extend*count[0][4];
+	dirsum+=(extend-1)*count[1][2]+(extend-1)*count[1][4];
+	dirsum+=count[3][0]+count[3][6];
+	dirsum+=count[6][3]+extend*count[6][2]+extend*count[6][4];
+	dirsum+=(extend-1)*count[5][2]+(extend-1)*count[5][4];
+
+	w=2*extend-1;
+	printf("Counting inside for w = %d\n", w);
+	dirsum+=w * count[3][3]; // pyramid bottom
+
+	for(w = w-2; w>0; w-=2) {
+		printf("Counting inside for w = %d\n", w);
+	 	dirsum+=w*count[3][3] + w*count[2][3];
+	}
+
+	printf("Reached %lld tiles.\n", dirsum);
+
+	printf("587655199212276 too low\n");
+	printf("596740523771076 too high\n");
 
 	return 0;
 }
