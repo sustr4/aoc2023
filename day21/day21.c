c26debb9d09115b7cd0fc20ff9fd4a125aee547e
Zdenek Sustr <git@sustr.net>
2023-12-21 13:44:17 +0100

    Day 21, Task 2

--- day21/day21.c
+++ day21/day21.c
@@ -16,25 +16,6 @@ int GOAL=196;
 //#define MAXY 11
 //#define GOAL 6
 
-// Point structure definition
-typedef struct {
-	int x;
-	int y;
-	int z;
-} TPoint;
-
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-	const int *da = (const int *) a;
-	const int *db = (const int *) b;
-	return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
 // Print a two-dimensional array
 void printDist (int **dist) {
 	int x,y;
@@ -56,8 +37,6 @@ void printMap (char **map) {
 		printf("\n");
 	}
 }
-// Full block character for maps █ and border elements ┃━┗┛┏┓
-// Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
 
 // Retrieve nth neighbor from a map
 int dy[] = { -1,  0, 0, 1 };
@@ -104,8 +83,6 @@ char **readInput() {
 	fclose(input);
 	if (line)
 	free(line);
-
-//	printMap(map);
 
 	return map;
 }
@@ -157,13 +134,6 @@ int main(int argc, char *argv[]) {
 			for(int x=0; x<MAXX*7; x++) if(dist[y][x]<=s) dist[y][x]=0;
 	}
 
-//	printDist(dist);
-
-//	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
-//	for(i=0; array[i]; i++) {
-//		printf("%d\n", array[i]);
-//	}
-
 	long long count[7][7];
 
 	int checksum=0;
@@ -185,66 +155,6 @@ int main(int argc, char *argv[]) {
 
 	printf("Now .. we need %d tiles up, down, left right...\n", extend);
 
-//	extend=3; // TODO: Remove, this is for checking
-
-
-	long long totsum = 0;
-
-	int w = 3; //width
-	for(int y=0-extend; y<=extend; y++) {
-		long long sum = 0;
-//		sum = 0; // TODO: Remove, this is only for testing
-		if(y==0-extend) { // The first line
-			if(w<10) printf("f ");
-			for(int x=2; x<5; x++) sum+=count[0][x];
-		}
-		else if(y<0) { // Other lines in top half
-			w+=2;
-			if((w+1)%4) { // even lines
-				if(w<10) printf("e ");
-				sum+=count[2][0]+count[2][1]+count[2][5]+count[2][6];
-				sum+=(w-4) / 2 * count[1][2]; // Center and twins
-				sum+=((w-4) / 2 + 1) * count[1][3]; // Wing and twins
-			}
-			else { // odd lines
-				if(w<10) printf("o ");
-				sum+=count[1][1]+count[1][2]+count[1][4]+count[1][5];
-				sum+=(w-4) / 2 * count[2][3]; // Center and twins
-				sum+=((w-4) / 2 + 1) * count[2][2]; // Wing and twins
-			}
-		}
-		else if(y==0) { // Center line
-			printf("m ");
-			sum+=count[3][0]+count[3][1]+count[3][5]+count[3][6];
-			sum+=(w-4)/2 * count[3][3];
-			sum+=((w-4)/2 + 1) * count[3][2];
-		}
-		else if(y<extend) {
-			if(y>1) w-=2;
-			if(y%2==0) { // even lines
-				if(w<10) printf("e ");
-				sum+=count[5][1]+count[5][2]+count[5][4]+count[5][5];
-				sum+=(w-4) / 2 * count[3][2]; // Center and twins
-				sum+=((w-4) / 2 + 1) * count[3][3]; // Wing and twins
-			}
-			else { // odd lines
-				if(w<10) printf("o ");
-				sum+=count[4][0]+count[4][1]+count[4][5]+count[4][6];
-				sum+=(w-4) / 2 * count[4][3]; // Center and twins
-				sum+=((w-4) / 2 + 1) * count[4][2]; // Wing and twins
-			}
-		} else if(y==extend) {
-			if(w<10) printf("l ");
-			w-=2;
-			for(int x=2; x<5; x++) sum+=count[6][x];
-		}
-
-		if((w<10) || (y==0)) printf("Width %2d, Line sum %lld\n", w, sum);
-		totsum+=sum;
-	}
-
-	printf("Reached %lld tiles.\n", totsum);
-
 	long long dirsum = 0;
 
 	dirsum+=count[0][3]+extend*count[0][2]+extend*count[0][4];
@@ -253,19 +163,12 @@ int main(int argc, char *argv[]) {
 	dirsum+=count[6][3]+extend*count[6][2]+extend*count[6][4];
 	dirsum+=(extend-1)*count[5][2]+(extend-1)*count[5][4];
 
-	w=2*extend-1;
-	printf("Counting inside for w = %d\n", w);
+	int w=2*extend-1;
 	dirsum+=w * count[3][3]; // pyramid bottom
 
-	for(w = w-2; w>0; w-=2) {
-		printf("Counting inside for w = %d\n", w);
-	 	dirsum+=w*count[3][3] + w*count[2][3];
-	}
+	for(w = w-2; w>0; w-=2) dirsum+=w*count[3][3] + w*count[2][3];
 
 	printf("Reached %lld tiles.\n", dirsum);
-
-	printf("587655199212276 too low\n");
-	printf("596740523771076 too high\n");
 
 	return 0;
 }
